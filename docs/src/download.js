009b1eb7a9bb1b2c0cccdc1af3de187489195cfc
Jerryh2-0 <supermighty0.cubeway2.mcpe@gmail.com>
2020-11-15 10:20:56 +0100

    added a french version of download page

---
+++ docs/src/download.js
@@ -0,0 +1,48 @@
+const queryString = window.location.search
+const urlParams = new URLSearchParams(queryString)
+
+if(urlParams.get('language')) language = urlParams.get('language')
+
+while(!translations) {
+
+}
+
+console.log(language)
+
+if(Object.keys(translations).indexOf(language) !== -1) {
+    translations = translations[language]
+    let title = document.getElementsByTagName('title')
+    title.textContent = translations.title
+
+    // navbar
+    document.querySelector("#button-home-page a").textContent = translations.homeLink
+    document.querySelector("#button-download-page a").textContent = translations.downloadLink
+
+    // presentation
+    document.querySelector("#presentation").textContent = translations.pagePresentation
+
+    // downloads
+    document.querySelectorAll(".recommended").forEach(elt => elt.textContent = translations.recommendedVersion)
+    document.querySelectorAll(".last").forEach(elt => elt.textContent = translations.lastVersion)
+
+    let i = 0
+    Array.from(document.querySelectorAll("#page p")).slice(1, 5).forEach(elt => {
+        if(typeof(translations["paragraphes"][i]) === "string") elt.childNodes[0].textContent = translations["paragraphes"][i]
+        else {
+            for(let j = 0; j < translations['paragraphes'][i].length; j++) {
+                elt.childNodes[j].textContent = translations['paragraphes'][i][j]
+            }
+        }
+        i++
+    })
+
+    // How to launch the game
+    Array.from(["windows", "mac"]).forEach(os => {
+        document.querySelector(`#${os}`).childNodes[1].textContent = translations["launching"][os]
+    })
+    document.querySelector('#linux').childNodes[1].textContent = translations["launching"]["linux"][0]
+    document.querySelector('#linux').childNodes[3].textContent = translations["launching"]["linux"][1]
+
+
+}
+
---
+++ docs/src/textContent/downloadTextContent.js
@@ -0,0 +1,33 @@
+let translations = {
+    fr: {
+        title: "Accueil - Formiko",
+        homeLink: "Accueil",
+        downloadLink: "Téléchargement",
+        pagePresentation: "Vous êtes sur la page de téléchargement ! Vous pouvez ici télécharger de nombreuses versions, pour Windows, MacOS, Linus ou même sans Java",
+        recommendedVersion: "Version recommandée :",
+        lastVersion: "Dernière version :",
+        withoutJava: "Version sans Java :",
+        paragraphes: [
+            [
+                "Vous pouvez d'autres versions ",
+                "ici"
+            ],
+            "Toutes les actions doivent être exécutée dans le dossier du jeu",
+            "Certains antivirus peuvent bloquer l'exécution de Java, il est donc nécessaire d'autoriser son exécution pour jouer au jeu",
+            [
+                "Si le nom de la version ne contient pas '(32 bit)', cela signifie qu'elle utilise une version 64 bit de Java. Si votre ordinateur est très ancien, vous aurez besoin d'une ",
+            "version 32 bit de Java ",
+            "pour jouer"
+            ],
+        ],
+        howToUse: "Comment jouer ?",
+        launching: {
+            windows: " Lancez le jeu en double cliquant sur formito.bat",
+            mac: " Lancez le jeu en double cliquant sur formiko.sh ou avec la commande ",
+            linux: [
+                " Avant de lancer, tapez : ",
+                "Puis, démarrer le jeu en double cliquant sur formiko.sh ou avec la commande "
+            ]
+        }
+    }
+}
