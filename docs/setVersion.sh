6386034dde99ca1e08e34177a621eeba5fa67af3
HydrolienF <hydrolien.f@gmail.com>
2020-12-21 20:45:46 +0100

    setVersion do an auto commit & a push to update web site.

--- docs/setVersion.sh
+++ docs/setVersion.sh
@@ -1,7 +1,6 @@
 #change version of the $lastStableVersion & $lastVersion in download.php.
 #./setVersion.sh \"1.15.2\" \"1.12.1\" or ./setVersion.sh \"1.15.3\"
 if [[ $# -eq 1 ]]; then
-  echo "1 seul agrs"
   echo "<?php \$lastStableVersion="$1"; \$lastVersion="$(cat version.md)" ?>" > download.php
 else
   echo $2 > version.md
@@ -9,3 +8,7 @@ else
 fi
 cat preDownload.php >> download.php
 php download.php > download.html
+rm download.php
+git add download.html
+git commit -m "auto releases for version "$1
+git push
