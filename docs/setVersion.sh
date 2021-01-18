c268dc5e3456fe73455d46c7c240e7846acdbc6b
HydrolienF <hydrolien.f@gmail.com>
2021-01-18 09:52:27 +0100

    satble & last change

--- docs/setVersion.sh
+++ docs/setVersion.sh
@@ -1,10 +1,10 @@
 #change version of the $lastStableVersion & $lastVersion in download.php.
-#./setVersion.sh \"1.12.1\" \"1.15.2\" or ./setVersion.sh \"1.15.3\"
+#./setVersion.sh \"1.15.2\" \"1.12.1\" or ./setVersion.sh \"1.15.3\"
 if [[ $# -eq 1 ]]; then
-  echo "<?php \$lastStableVersion="$1"; \$lastVersion="$(cat version.md)" ?>" > download.php
+  echo "<?php \$lastVersion="$1"; \$lastStableVersion="$(cat version.md)" ?>" > download.php
 else
   echo $2 > version.md
-  echo "<?php \$lastStableVersion="$1"; \$lastVersion="$2" ?>" > download.php
+  echo "<?php \$lastVersion="$1"; \$lastStableVersion="$2" ?>" > download.php
 fi
 cat preDownload.php >> download.php
 php download.php > download.html
