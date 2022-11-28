a3d399d4aad700f121774618d03adf97829d36b8
HydrolienF <hydrolien.f@gmail.com>
2022-11-28 11:30:02 +0100

    Add kokcinelo.html (redirection & file)

---
+++ docs/src/getEndFile.js
@@ -0,0 +1,14 @@
+function getEndFileName(){
+  var os = navigator.oscpu || navigator.platform
+  os=os.toLowerCase();
+  if(os.includes("win")){
+    return "msi"
+  }else if(os.includes("linux")){
+    return "deb"
+  }else if(os.includes("mac")){
+    return "dmg";
+  }else{
+    return "zip";
+  }
+}
+document.getElementById("lastStableVersionChooseOs").href = "https://github.com/HydrolienF/Kokcinelo/releases/latest/download/Kokcinelo."+getEndFileName();
