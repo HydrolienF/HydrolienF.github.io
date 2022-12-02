54e46e555cd18c75bbfd0c1e828b7e042b2c5f81
HydrolienF <hydrolien.f@gmail.com>
2022-12-02 09:42:45 +0100

    hide youtube trailler

--- docs/src/getEndFile.js
+++ docs/src/getEndFile.js
@@ -1,14 +1,14 @@
-function getEndFileName(){
+function getEndFileName() {
   var os = navigator.oscpu || navigator.platform
-  os=os.toLowerCase();
-  if(os.includes("win")){
-    return "msi"
-  }else if(os.includes("linux")){
+  os = os.toLowerCase();
+  if (os.includes("win")) {
+    return "exe"
+  } else if (os.includes("linux")) {
     return "deb"
-  }else if(os.includes("mac")){
+  } else if (os.includes("mac")) {
     return "dmg";
-  }else{
+  } else {
     return "zip";
   }
 }
-document.getElementById("lastStableVersionChooseOs").href = "https://github.com/HydrolienF/Kokcinelo/releases/latest/download/Kokcinelo."+getEndFileName();
+document.getElementById("lastStableVersionChooseOs").href = "https://github.com/HydrolienF/Kokcinelo/releases/latest/download/Kokcinelo." + getEndFileName();
